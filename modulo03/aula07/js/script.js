d6f74158ddb443d7227a7a2dd928870b924d8b28
bernardobru <tutibruxel@gmail.com>
2024-06-04 13:52:07 -0300

    adicionados arquivos da aula

---
+++ modulo03/aula07/js/script.js
@@ -0,0 +1,8 @@
+/* alert('teste'); */
+/* document.getElementById('nome').value = 'Bernardo'; */
+let numero = prompt("Digite um número: ");
+if (numero % 2 === 0) {
+    document.write('<h1>O número é par</h1>');
+} else {
+    document.write('<h1>O número é ímpar</h1>');
+}
