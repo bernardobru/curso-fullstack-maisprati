50c73aaae9aa120b82f7a6bb3fd17f73096046ef
Bernardo Bruxel <tutibruxel@gmail.com>
2024-07-20 14:33:45 -0300

    added challenge aula 07

--- aulas-modulo04/aula07/contextAPI/src/App.jsx
+++ aulas-modulo04/aula07/contextAPI/src/App.jsx
@@ -1,28 +1,36 @@
 /* import MyChildren from './components/Children'; */
 import { createContext, useContext, useState } from 'react';    
 //Padrão de contexto
-const ThemeContext = createContext({theme: 'light', toggleTheme: () => {}});
+const PreferencesContext = createContext({theme: 'light', lang: 'en'});
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [preferences, setPreferences] = useState({theme: 'light', lang: 'en'});
   const toggleTheme = () => {
-      setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light');
+    setPreferences(preference =>  ({
+      theme: preference.theme === 'light' ? 'dark' : 'light'
+    }));
+  }
+  const toggleLang = (lang) => {
+    setPreferences((currentLang) => ({
+      ...currentLang, lang: lang
+    }));
   }
   return(
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-          <div style={{backgroundColor: theme === 'dark' ? '#333333' : '#fdfdfd'}}>
-            <ToolBar/>
-            <button onClick={toggleTheme}>Alternar tema</button>
-          </div>
-        </ThemeContext.Provider>
+    <PreferencesContext.Provider value={{preferences, toggleTheme, toggleLang}}>
+      <div style={{backgroundColor: preferences.theme === 'dark' ? '#333333' : '#fdfdfd'}}>
+        <ToolBar/>
+        <button onClick={toggleTheme}>Trocar tema</button>
+        <button onClick={() => toggleLang('pt-br')}>Trocar idioma para português</button>
+        <button onClick={() => toggleLang('en')}>Trocar idioma para inglês</button>
+      </div>
+    </PreferencesContext.Provider>
   );
 }
 function ToolBar() {
-    const {theme} = useContext(ThemeContext);
-    return(
-        <div>
-            Tema: {theme}
-        </div>
-    )
+  const {preferences} = useContext(PreferencesContext);
+  return(
+    <div style={{backgroundColor: preferences.theme === 'dark' ? '#454545' : '#fcfcfc', color: preferences.theme === 'dark' ? '#ffffff' : '#000000'}}>
+        Preferências: Tema: {preferences.theme} e Idioma: {preferences.lang}
+    </div>
+  )
 }
-
 export default App;
