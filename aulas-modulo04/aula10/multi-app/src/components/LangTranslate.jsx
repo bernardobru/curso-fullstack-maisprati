22a926f7a1c3ad17fcffd8a0392303a4ae8fb8d8
bernardobru <tutibruxel@gmail.com>
2024-07-29 11:21:19 -0300

    started aula 10

---
+++ aulas-modulo04/aula10/multi-app/src/components/LangTranslate.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import axios from 'axios';
+function LangTranslate() {
+
+    return(
+        <Container>
+            <Title>
+                <div>
+                    
+                </div>
+            </Title>
+        </Container>
+    );
+}
+export default LangTranslate;
